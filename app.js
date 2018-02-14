5fc0178208133905b31aa91a13bac4580c29b10b
ibrahimyolbir <ibrahimyolbirr@gmail.com>
2018-02-14 11:00:08 +0100

    added nya coder

---
+++ app.js
@@ -0,0 +1,26 @@
+//String
+var name =`Ibrahim`;
+//Number
+var number = 500;
+
+//Null
+var housesomeday = null;
+
+//Undefined
+var walking = undefined;
+// Boolean
+var lazy = true;
+//array
+var ShoppingList=[name,number,housesomeday,walking,lazy];
+console.log(person);
+console.log(ShoppingList);
+//Object
+var person = {
+    name:name,
+    pay:number,
+    house:housesomeday,
+    excersise:walking,
+    lazy:lazy
+};
+console.log(person);
+
